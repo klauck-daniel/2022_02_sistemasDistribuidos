51cc51b844f794f84148228de55924e6b733fa0d
klauck-daniel <97921706+klauck-daniel@users.noreply.github.com>
2022-09-24 17:12:00 -0300

    Faltando gravar log

--- rtos_ga/rtos_ga.c
+++ rtos_ga/rtos_ga.c
@@ -33,23 +33,23 @@ int main()
     pthread_join( ts1, NULL);
     pthread_join( ts2, NULL);
     pthread_join( ta, NULL);
-
 }
 
 void* ts1_function(void *arg)
 {
+    srand((unsigned) time(&t));
     /* Bloqueia a mutex para garantir 
 	acesso exclusivo a variavel count. 
 	Se a mutex já estiver bloqueada, a thread
 	vai ficar bloqueda também.
 	*/
-    srand((unsigned) time(&t));
     pthread_mutex_lock(&mutex1);
     for(count_ts1 = 0; count_ts1 < 5; count_ts1 ++)
     {
       vs1[count_ts1] = (-8 + rand()%50)/(1.98+count_ts1);
       printf("VS1[%d] = %.2f\n", count_ts1, vs1[count_ts1]);
     }
+    
 	//Desbloqueia a mutex.
 	pthread_mutex_unlock(&mutex1);
 }
@@ -58,7 +58,7 @@ void* ts2_function(void *arg)
 {
     /*sleep para mudar o horário da máquina e
     gerar novo seed para a srand()*/
-    sleep(3);
+    sleep(1);
     srand((unsigned) time(&t));
     pthread_mutex_lock(&mutex2);
     for(count_ts2 = 0; count_ts2 < 5; count_ts2 ++)
@@ -69,7 +69,31 @@ void* ts2_function(void *arg)
 	pthread_mutex_unlock(&mutex2);
 }
 
-void* ta_function(void *arg){
+void* ta_function(void *arg)
+{
+    //Inicialização da função de leitura de arquivo
+    FILE *pArq = NULL;
+    pArq = fopen("logS1.txt","a");
+        if(pArq == NULL) 
+    {
+        printf("Erro ao abrir arquivo.\n");
+        return 0;
+    }
     
+   for(int i = 0; i < 5; i++)
+   {
+    if(vs1[i] < 0)
+    {
+        printf("DESCARTAR VS1[%d] = %.2f\n", i, vs1[i]);
+        // Escreve a mensagem no arquivo
+        fprintf(pArq, "%s",  "Valor menor que zero! - ");
+        fprintf(pArq, "%f - ",  vs1[i]);
+        fprintf(pArq, "%s\n"), ctime (&t);
+    }
+       
+   } 
 }
-	
+
+
+pArq = fopen("logS2.txt","a");
+
