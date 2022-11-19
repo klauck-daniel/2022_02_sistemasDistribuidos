faa8832c17e60fb2ffc48ab298d954e2d829195f
Daniel Klauck <daniel_klauck@hotmail.com>
2022-11-19 11:02:10 -0300

    implementação do teste de publicação do paho mqtt

--- trabalho_final_GB/stockIncoming.py
+++ trabalho_final_GB/stockIncoming.py
@@ -1,1 +1,20 @@
-#Aplicativo que envia a mensagem MQTT quando alguma mercadoria ENTRA no estoque
+# Aplicativo que publica a mensagem MQTT quando alguma mercadoria ENTRA no estoque
+
+import paho.mqtt.client as paho
+import sys
+import socket
+import time
+
+# instancia o paho client
+client = paho.Client()
+
+# cria a conexão com o broker e verifica se a conexão deu certo
+if client.connect("localhost", 1883, 60) != 0:
+    print("Not conected do MQTT broker!")
+    sys.exit(-1)
+
+#ARGUMENTOS(tópico, mensagem de payload, nível de qualidade do serviço)
+client.publish("test/status", "Helloworld from paho-mqtt!", 0)
+
+#desconecta do broekr
+client.disconnect()
